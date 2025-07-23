af2a61ae469de1eb2d025b88f43deda11013c236
msisaif <msi313.bd@gmail.com>
2025-07-23 21:56:30 +0600

    Income complete

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Income extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public function incomeHead()
+    {
+        return $this->belongsTo(IncomeExpenseHead::class, 'income_expense_head_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
